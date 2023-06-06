924288ebab4e8730c1243ee98e9009d41c37344e
cartermusee <cartermusee@gmail.com>
2023-06-06 09:05:28 -0700

    check if its greater than 5 or less than 6 or zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-mod = number % 10
+if number >= 0:
+    mod = number % 10
+else:
+    mod = number % -10
 if mod > 5:
     print(f"last digit of {number} is {mod} and is greater than 5")
 elif mod == 0:
