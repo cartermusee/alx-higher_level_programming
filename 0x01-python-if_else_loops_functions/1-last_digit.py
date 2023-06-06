7462fd1964a104e163d5bfcae720925ef716507d
cartermusee <cartermusee@gmail.com>
2023-06-06 09:09:49 -0700

    check if its greater than 5 or less than 6 or zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,10 @@ if number >= 0:
     mod = number % 10
 else:
     mod = number % -10
+print(f"last digit of {number} is {mod}",end=' ')
 if mod > 5:
-    print(f"last digit of {number} is {mod} and is greater than 5")
+    print(f"and is greater than 5")
 elif mod == 0:
-    print(f"last digit of {number} is {mod} and is 0")
+    print(f"and is 0")
 else:
-    print(f"last digit of {number} is {mod} and is less than 6 and not 0")
+    print(f"and is less than 6 and not 0")
