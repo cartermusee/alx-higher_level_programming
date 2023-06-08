68c81a5fa2fcc0c4e65674c0cf3fdb115cf34c2b
cartermusee <cartermusee@gmail.com>
2023-06-08 03:13:28 -0700

    import arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """ numbers of a list."""
+    import sys
+
+    count_len = len(sys.argv) - 1
+    if count_len ==0:
+        print('0 arguments.')
+    elif count_len == 1:
+        print('1 argument:')
+    else:
+        print('{} arguments:'.format(count_len))
+    for i in range(count_len):
+        print('{}: {}'.format(i + 1, sys.argv[i + 1))
