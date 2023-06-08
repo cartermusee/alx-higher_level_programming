fc86cff8d365fd0e6085842c931265a52766ea11
cartermusee <cartermusee@gmail.com>
2023-06-08 03:34:12 -0700

    import arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+
+
 if __name__ == "__main__":
-    """ numbers of a list."""
+    """numbers of a list."""
     import sys
 
-    count_len = len(sys.argv) - 1
-    if count_len == 0:
-        print('0 arguments:')
-    elif count_len == 1:
+    c = len(sys.argv) - 1
+    if c == 0:
+        print('0 arguments.')
+    elif c == 1:
         print('1 argument:')
     else:
-        print('{} arguments:'.format(count_len))
-    for i in range(count_len):
-        print('{}: {}'.format(i + 1, sys.argv[i + 1)))
+        print('{} arguments:'.format(c))
+    for i in range(c):
+        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
