0ed516f6a2ff7cbed5d4f88eb1891804353b446b
cartermusee <cartermusee@gmail.com>
2023-06-08 03:46:12 -0700

    import 4-

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """all names defined by hidden module."""
+    import hidden_4
+
+    nm = dir(hidden_4)
+    for i in nm:
+        if name[:2] != "__":
+            print(nm)
