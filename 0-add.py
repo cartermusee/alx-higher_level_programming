8cb5f5f17b7ac9c09b961d06bd4f83ae8e4358f5
cartermusee <cartermusee@gmail.com>
2023-06-08 02:48:31 -0700

    import add

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bi/python3
+if __name__ == "__main__":
+
+    """ sum of two numbers """
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
