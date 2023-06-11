2f22c9b9dfb78764c422204f22756ad08eaa7753
cartermusee <cartermusee@gmail.com>
2023-06-11 11:12:19 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     ln = len(my_string) - 1
     new = ""
     for i in range(ln):
-        if my_string[i] != 'c' and  my_string[i] != 'C':
+        if my_string[i] != 'c' and my_string[i] != 'C':
             new += my_string[i]
-    return new 
+    return new
