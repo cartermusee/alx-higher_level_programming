dec29225ac88bbb4dda28c9c553bd2b6d007eae2
cartermusee <cartermusee@gmail.com>
2023-06-12 01:21:18 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,6 +6,6 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for i in my_list:
         if my_list[i] % 2 == 0:
-            new_list.append(true)
+            new_list.append(True)
         else:
-            new_list.append(false)
+            new_list.append(False)
