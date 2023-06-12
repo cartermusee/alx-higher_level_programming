b32f8c03ce2586852aa4c579ad3e284680f5ddca
cartermusee <cartermusee@gmail.com>
2023-06-12 00:39:52 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    length = len(my_list)
+    max_value = my_list[0]
+    for i in range(length):
+        if my_lsit[i] > max_value:
+            max_value = my_list[i]
+    return max_value
