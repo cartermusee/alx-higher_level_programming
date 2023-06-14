472593bf3e103998cd9557b95a7400d30fd6d8ce
cartermusee <cartermusee@gmail.com>
2023-06-14 06:28:59 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def mul(matrix =[]):
-    return list(map(lambda x : list(map(lambda f : f ** 2, x)), matrix))
+def square_matrix_simple(matrix=[]):
+    return list(map(lambda x: list(map(lambda f: f ** 2, x)), matrix))
