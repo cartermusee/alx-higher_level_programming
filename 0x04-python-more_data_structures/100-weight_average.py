4836e3b122ae7311842a4849c3897885e8fc5c16
cartermusee <cartermusee@gmail.com>
2023-06-14 11:48:21 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    ave =0
-    div =0
+    ave = 0
+    div = 0
     for i in my_list:
         ave += i[0] * i[1]
-    return float(ave/div)
+    return float(ave / div)
