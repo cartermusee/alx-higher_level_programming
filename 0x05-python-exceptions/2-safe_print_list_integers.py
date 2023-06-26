22aa229019b4d46775e15c40fb15bccc3ee846f8
cartermusee <cartermusee@gmail.com>
2023-06-26 08:22:59 -0700

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            n += 1
+    print("")
+    return(n)
