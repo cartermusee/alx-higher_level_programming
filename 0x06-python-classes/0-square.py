4858fac68c1f36161d551e1922fa6096a90ee0f7
cartermusee <cartermusee@gmail.com>
2023-06-27 09:54:47 -0700

    1-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ class square is an empty class with the name square"""
+
+
 class Square:
     """ a squre"""
     pass
-
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" define a class square"""
+
+
+class Square:
+    """ a square """
+
+    def __init__(self, size)
+    """ initialize a new one (square)
+    takes arg:
+    size which is int and size of new square"""
+
+    self.__size = size
