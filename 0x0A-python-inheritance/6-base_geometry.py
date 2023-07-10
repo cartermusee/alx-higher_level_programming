4a5898d5756ab484837a61a809d7bb6be0b1a572
cartermusee <cartermusee@gmail.com>
2023-07-10 05:48:58 -0700

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module for class basegeo"""
+
+
+class BaseGeometry:
+    """a class with method area"""
+    def area(self):
+        """function wich raises error"""
+        raise Exception("area() is not implemented")
