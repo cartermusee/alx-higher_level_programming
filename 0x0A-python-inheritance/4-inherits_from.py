14264d398b4ee589ea2b6da7007d73e460bedd2c
cartermusee <cartermusee@gmail.com>
2023-07-10 05:43:13 -0700

    issubclass function

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """to check if subclass inherited"""
-    return issubclass(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
