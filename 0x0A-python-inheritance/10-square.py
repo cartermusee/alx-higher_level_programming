1194e8b04bdf4b17a090828b3d39db72dbcfb330
cartermusee <cartermusee@gmail.com>
2023-07-10 06:53:13 -0700

    square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,9 +6,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """class which inherits from class rectangle"""
     def __init__(self, size):
+        """function for variables"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        """return area"""
         return self.__size ** 2
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,6 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """ class rectangle which nherits geometry"""
     def __init__(self, width, height):
+        """function for variable"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
