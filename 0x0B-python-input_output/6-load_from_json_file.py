f2b3fff9c4db4b0ccbbb75857a823fb861dfa4e4
cartermusee <cartermusee@gmail.com>
2023-07-11 07:08:21 -0700

    read json to py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """function to creates an Object from a jSON file"""
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
