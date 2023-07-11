52e4381d701fafbdd320f3c6079a9d6767e9a515
cartermusee <cartermusee@gmail.com>
2023-07-11 06:18:04 -0700

    writing a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """function for writing a file"""
 
     with open(filename, 'w', encoding="utf-8") as f:
-        print(f.write(text), end='')
+        return f.write(text)
