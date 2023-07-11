c47bf03f3af646ebef4c0eb5c8e7d373cb8622f7
cartermusee <cartermusee@gmail.com>
2023-07-11 06:03:20 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename = ""):
     """function which reads a file"""
-    with open(filename, encoding = 'utf-8') as f:
+    with open(filename, 'r',  encoding = 'utf-8') as f:
         print(f.read())
 
