a2400a650823ae0663602b40610d2d9be8910f10
cartermusee <cartermusee@gmail.com>
2023-08-16 11:33:17 -0700

    use of left outer join

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -6,5 +6,5 @@ ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name LIKE '%Comedy%'
-ORDER BY tv_shows.title ASC
+ORDER BY tv_shows.title ASC;
 ~                            
