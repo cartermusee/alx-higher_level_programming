d0423a59a9adaf566b6647c6990cb42296e87b01
cartermusee <cartermusee@gmail.com>
2023-08-17 07:53:53 -0700

    temperature

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
 -- change to utf-8
 USE `hbtn_0c_0`
-ALTER DATABASE `hbtn_0c_0`CHARACTER SET tf8mb4 collate utf8mb4_unicode_ci;
-SELECT * FROM `first_table`
-ALTER TABLE `first_table` MODIFY name CHARACTER SET tf8mb4 collate utf8mb4_unicode_ci;
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET tf8mb4 COLLATE utf8mb4_unicode_ci;
