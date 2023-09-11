99425437f5e5ea649b3c9d7321f0d4d0ac13a8a7
cartermusee <cartermusee@gmail.com>
2023-09-11 13:50:48 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const languageC = 'C is fun';
+const languagePython = 'Python is cool';
+const languageJs = 'JavaScript is amazing';
+
+console.log(languageC);
+console.log(languagePython);
+console.log(languageJs);
