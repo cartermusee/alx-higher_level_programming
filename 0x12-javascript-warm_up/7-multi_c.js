09aa766dbd784073c0c9d5c4cb8d3aaa728c48b2
cartermusee <cartermusee@gmail.com>
2023-09-11 15:04:57 -0700

    advance

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const x = process.argv[2];
 const num1 = parseInt(x);
 if (num1) {
   for (let i = 0; i < num1; i++) {
-    console.log('c is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
