4de661fc482b1e3bb4be3b430f559fe5382965b8
cartermusee <cartermusee@gmail.com>
2023-09-11 14:50:05 -0700

    13-add.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length < 4) {
-    console.log('0');
-  } else {
-    const data = process.argv.slice(2);
-    data.sort((i, j) => j - i);
-    console.log(data[1]);
-  }
+  console.log('0');
+} else {
+  const data = process.argv.slice(2);
+  data.sort((i, j) => j - i);
+  console.log(data[1]);
+}
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const x = process.argv[2];
 const num1 = parseInt(x);
-if (num1){
-    for (let i = 0; i < x; i++){
-        console.log('c is fun');
-    }
-}
-else{
-    console.log('Missing number of occurrences')
+if (num1) {
+  for (let i = 0; i < x; i++) {
+    console.log('c is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
