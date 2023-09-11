68b2d329c9263a583f9197cef1afb7b48e4dd242
cartermusee <cartermusee@gmail.com>
2023-09-11 14:23:41 -0700

    javascript process.argv

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const x = process.argv[2];
 const num1 = parseInt(x);
-if (isNaN(num1)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('c is fun');
-  }
+if (parseInt(num1)){
+    for (let i = 0; i < x; i++){
+        console.log('c is fun');
+    }
+}
+else{
+    console.log('Missing number of occurrences')
 }
