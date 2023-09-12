dffcd0f98a161b6fdf7357872ae181093ebd8a6e
cartermusee <cartermusee@gmail.com>
2023-09-11 23:26:08 -0700

    advanced-correction more on require node js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myLines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myLines.length; i++) {
   console.log(myLines[i]);
 }
