9beed252b9b288d20d4a4a7b4a2fa9bd1a8cb0da
cartermusee <cartermusee@gmail.com>
2023-09-16 01:40:56 -0700

    map

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   function Base (n) {
-    if (n < base) {
-      return n.toString(base);
-    } else {
-      return Base(Math.floor(n / base)) + (n % base).toString();
-    }
+    return n.toString(base);
   }
 
   return Base;
