82b49d37033cdaec033ec12f7f6f1805275f81ac
cartermusee <cartermusee@gmail.com>
2023-09-17 03:20:39 -0700

    all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,10 @@
 script that lists all
 states from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
 from sys import argv
+
 
 if __name__ == '__main__':
     """
@@ -13,11 +15,11 @@ if __name__ == '__main__':
     """
 
     db_connection = MySQLdb.connect(
-        host="locahlhost",port=3306,user=argv[1],
-        passwd=argv[2],db=argv[3])
+        host="localhost", port=3306, user=argv[1],
+        passwd=argv[2], db=argv[3])
     cur = db_connection.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id ASC)")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
     results = cur.fetchall()
 
