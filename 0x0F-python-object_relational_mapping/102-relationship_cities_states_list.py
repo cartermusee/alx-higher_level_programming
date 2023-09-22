364a9f92aaff33e7ca1ad26dddd615deb2e681f5
cartermusee <cartermusee@gmail.com>
2023-09-22 14:20:17 -0700

    102-relationship_cities_states_list.py

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script 14-model_city_fetch_by_state.py
+that prints all City objects
+from the database hbtn_0e_14_usa
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+from model_city import City
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost: \
+                        3306/{}".format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    cn_session = sessionmaker(bind=engine)
+    session = cn_session()
+    query = session.query(State).join(City).\
+        order_by(City.id).all()
+    for state in query:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
+    session.commit()
+    session.close()
