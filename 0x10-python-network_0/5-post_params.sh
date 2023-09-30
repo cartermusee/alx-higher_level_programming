94c9fc099ba4c12eba5eb0e4a57cd9b90cd0dad3
cartermusee <cartermusee@gmail.com>
2023-09-30 12:51:13 -0700

    solved 3 4 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a POST request to
-curl -s -Xd POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
