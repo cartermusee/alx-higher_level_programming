3e4fc81718e5e40b740b9c604f3479e7a28b15b6
cartermusee <cartermusee@gmail.com>
2023-09-30 11:45:43 -0700

    sending a request and display size of response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,8 +2,7 @@
 #Bash script that takes in a URL, sends a request
 #to that URL, and displays the size of the body of the response
 
-url=$1
 
-response=$(curl -s "$url")
+response=$(curl -s "$1")
 size=${#response}
 echo "$size"
