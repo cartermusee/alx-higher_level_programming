8ab4a10277f028d3b2090b4f9c75ef7a320fb5d7
cartermusee <cartermusee@gmail.com>
2023-10-02 16:02:58 -0700

    netw

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,15 @@
 """module for request"""
 
 
-import urliib.request
+import urllib.request
 
 
 if __name__ == "__main__":
-    reg = urllib.requestRequest("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(reg) as res:
+    reg = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(reg) as response:
         res = response.read()
 
-        print("Body response:$")
-        print("\t- type: {}".formta(type(res)))
-        print("\t- content: {}".formta(res))
-        print("\t- utf8 content: {}".formta(res.decode("utf-8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode("utf-8")))
