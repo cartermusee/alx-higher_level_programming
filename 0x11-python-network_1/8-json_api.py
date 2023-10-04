9dea9222336484620324f36e11ae3b83f1952a90
cartermusee <cartermusee@gmail.com>
2023-10-04 11:21:18 -0700

    solved

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module for request package"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    par = {"q": "q"}
+    try:
+        res = requests.post(url, data=par)
+        res_json = res.json()
+
+        if res_json:
+            print("{[]} {}".format(res_json['id'], res_json['name']))
+        else:
+            print("No results")
+    except ValueError:
+        print("Not a valid JSON")
