3f9ec0549a80507cef8e5548dad2c05cb03c50d6
cartermusee <cartermusee@gmail.com>
2023-10-04 10:27:05 -0700

    request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 
 res = requests.get('https://alx-intranet.hbtn.io/status')
 print("Body response:")
-print("\t-type: {}".format(type(res.text)))
-print("\t-content: {}".format(res.text))
+print("- type: {}".format(type(res.text)))
+print("- content: {}".format(res.text))
