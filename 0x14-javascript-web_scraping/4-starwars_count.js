aa7ff94c95d662ece382cd82c0f8ac0f517aa58d
cartermusee <cartermusee@gmail.com>
2023-10-24 15:29:30 -0700

    requests

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = '18';
+let count = 0;
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (!error) {
+    const films = JSON.parse(body).results;
+    for (const film of films) {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
+        count++;
+      }
+    }
+    console.log(count);
+  } else {
+    console.error(error);
+  }
+});
