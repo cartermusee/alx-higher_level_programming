44789fde3d4d779843bae5bdf739bef88dc9f70d
cartermusee <cartermusee@gmail.com>
2023-10-24 15:32:52 -0700

    requests

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (!err) {
+    fs.writeFile(file, body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
