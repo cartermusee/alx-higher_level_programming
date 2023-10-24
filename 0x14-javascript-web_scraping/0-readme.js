bd21f57c71f7229046bcdb97bd52e8e53fae2e33
cartermusee <cartermusee@gmail.com>
2023-10-24 12:59:57 -0700

    node

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 const path = process.argv[2];
-fs.readFile(path,'utf8', (err, file)=>{
-    if (err){
-        console.error(err);
-    }
-    else{
-        console.log(file);
-    }
+fs.readFile(path, 'utf8', (err, file) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(file);
+  }
 });
