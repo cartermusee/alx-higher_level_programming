dced57fd526b45beb3d03477569704c20ba114a9
cartermusee <cartermusee@gmail.com>
2023-10-25 02:14:22 -0700

    request

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2]
+const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (err, response, body) => {
   if (!err) {
